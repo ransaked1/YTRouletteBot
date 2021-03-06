8579ef4b5f7022ef28a3e5db26ba7a04de075d12
dbraghis <danbraghis@yahoo.com>
2021-03-06 00:42:12 +0000

    always on

--- randomYT.py
+++ randomYT.py
@@ -2,8 +2,10 @@ import requests
 import os
 import discord
 from dotenv import load_dotenv
-import random
 from bs4 import BeautifulSoup
+from stayup import keep_alive
+
+keep_alive()
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
